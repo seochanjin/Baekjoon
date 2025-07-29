a414279742c2e4300f56e6c0736cc303f5e4c79d
seochanjin <scj1507@gmail.com>
2025-07-29 10:58:14 +0900

    bfs(중): 다익스트라 사용해야함. 최소 거리 구할때는 다익스트라 사용하는데 bfs아닌거 같은데?

---
+++ WEEK3/1916.py
@@ -0,0 +1,35 @@
+import sys
+from collections import defaultdict, deque
+import heapq
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+edge = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+start, end = map(int, sys.stdin.readline().split())
+
+print(edge)
+print(start, end)
+
+adj = defaultdict(list)
+
+for v, u, w in edge:
+    adj[v].append((u, w))
+
+
+result = 0
+INF = int(1e9)
+cnt=[INF]*(n+1)
+cnt[start] = 0
+heap = [(0, start)]
+
+while heap:
+    cost, node = heapq.heappop(heap)
+
+    if cnt[node] < cost:
+        continue
+    for a, b in adj[node]:
+        if cnt[a] > cost + b:
+            cnt[a] = cost + b
+            heapq.heappush(heap, (cnt[a], a))
+
+print(cnt[end])
